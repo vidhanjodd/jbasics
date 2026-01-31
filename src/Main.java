eb6c6f256e4a76157f2a6c515dd2b0aaa95be676
Vidhan <vidhanlonare@gmail.com>
2026-01-31 16:21:48 +0530

    added int z

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main { public static void main(String[] args) {
         int x =10;
         int y =20;
-        
+        int z =0;
         System.out.println("Hello, World!");
         System.out.println(x+y);
         System.out.println(x*y);
+        System.out.println(z);
 
     }
 
