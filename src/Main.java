2059ba78128c11856c68a8c7b3181554b876f0b9
Vidhan Lonare <134129801+vidhanjodd@users.noreply.github.com>
2026-01-31 16:10:46 +0530

    Add variables x and y to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main { public static void main(String[] args) {
-
+        int x =10;
+        int y =20;
+        
         System.out.println("Hello, World!");
-
+        System.out.println(x+y);
+        
 
     }
 
